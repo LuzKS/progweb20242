ea30cc176f43d46a9cc0a121ebd9c9e805346a46
Lucas Santos <161741045+LuzKS@users.noreply.github.com>
2024-09-26 12:52:34 -0300

    Add files via upload

---
+++ atvd2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex 1</title>
+</head>
+<body>
+    <?php
+    echo '<h1>URLs Absolutas</h1>';
+    echo '<a href="https://pt.stardewvalleywiki.com/Stardew_Valley_Wiki">StardewWiki</a>';
+    echo '</br>';
+    echo '<a href="https://ava3.cefor.ifes.edu.br/my/"> ava3</a>';
+
+
+
+    echo '<h1>URLs Relativas</h1>';
+    echo '<a href="receita.html"> receita</a>';
+    echo '</br>';
+    echo '<a href="infos.html"> infos</a>';
+
+    ?>
+    
+</body>
+</html>
