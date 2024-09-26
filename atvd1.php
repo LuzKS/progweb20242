a8a8066b8ae26b71b3192697f08ba4a0dd146a26
Lucas Santos <161741045+LuzKS@users.noreply.github.com>
2024-09-26 12:21:07 -0300

    Add files via upload

---
+++ atvd1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex 1</title>
+</head>
+<body>
+    <?php
+    echo '<img src=https://picsum.photos/500/600> <br></br>';
+    echo '<p>Imagem da Semana 2</p>';
+
+    ?>
+</body>
+</html>
